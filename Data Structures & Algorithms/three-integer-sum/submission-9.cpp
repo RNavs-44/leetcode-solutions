16b9672d5f1aa774dacbce6f68c9c358572d6096
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2026-08-03 15:54:38 +0100

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); // O (n log n)
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            // 2 sum, target is -nums[i]
+            int l = i + 1; int r = nums.size() - 1;
+            int target = nums[i] * (-1);
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum == target) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    while (l < r && nums[l] == nums[l-1]) {
+                        l++;
+                    }
+                }
+                else if (sum < target) {
+                    l++;
+                }
+                else {
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+};
