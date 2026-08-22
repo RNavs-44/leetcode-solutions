5fe3ac2601e3d0dbddc7cd580c680b7ff5bb137d
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2026-08-22 16:46:31 +0530

    Add: last-stone-weight - submission-8

---
+++ Data Structures & Algorithms/last-stone-weight/submission-8.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap (stones.begin(), stones.end());
+        while (maxHeap.size() > 1) {
+            int x = maxHeap.top(); maxHeap.pop();
+            int y = maxHeap.top(); maxHeap.pop();
+            if (x > y) maxHeap.push(x - y);
+        }
+        if (maxHeap.empty()) return 0;
+        return maxHeap.top();
+    }
+};
